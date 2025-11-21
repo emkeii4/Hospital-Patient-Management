5481dbc1258ef9a4a61a61aae02e3d6e64be4069
alisondeniz26-dev <alisondeniz26@gmail.com>
2025-11-21 10:05:06 +0000

    Update Hospital-Patient-Management.py

--- Hospital-Patient-Management.py
+++ Hospital-Patient-Management.py
@@ -79,7 +79,6 @@ def main_function(registration_of_patients: PatientsList, doctors_staff: Doctors
     result: List[str] = []
     for patient in registration_of_patients:
         name, age, gender, symptoms = patient
-        symptom = symptoms[0]
         needed_speciality, priority = speciality_by_symptoms(patient)
         chosen_doctor = doctors_by_speciality(doctors_staff, needed_speciality)
       
